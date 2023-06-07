fc7b72919bbbf2b6e8a2860ae618a6bbe03082e1
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-06-08 00:40:53 +0100

    error message added to login where password and username don't match

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -4,6 +4,8 @@ import './LoginForm.css';
 const LogInForm = ({ navigate }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+  const [shakeEffect, setShakeEffect] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -17,33 +19,39 @@ const LogInForm = ({ navigate }) => {
     });
 
     if (response.status !== 201) {
-      console.log("yay");
-      navigate("/login");
-    } else {
-      console.log("oop");
-      
-      let data = await response.json();
-      window.localStorage.setItem("token", data.token);
-      window.localStorage.setItem("username", username)
-
-      let avatarResponse = await fetch(`/users/${username}/avatars`)
-      let avatarData = await avatarResponse.json()
-      window.localStorage.setItem("avatar", avatarData.avatar)
-
-      const expirationDate = new Date();
-      expirationDate.setDate(
-        expirationDate.getDate() + 1 * 24 * 60 * 60 * 1000
-      );
-
-      let cookieValue =
-        encodeURIComponent("token") + "=" + encodeURIComponent(data.token);
-      cookieValue += "; expires=" + expirationDate.toUTCString();
-      cookieValue += "; path=/"; // Optional: set the cookie path
-
-      document.cookie = cookieValue;
-
-      navigate("/posts");
+      setLoginError(true);
+      setShakeEffect(true);
+      setUsername("");
+      setPassword("");
+      setTimeout(() => {
+        setShakeEffect(false);
+      }, 500);
+      return;
     }
+
+    console.log("Login successful!");
+
+    let data = await response.json();
+    window.localStorage.setItem("token", data.token);
+    window.localStorage.setItem("username", username);
+
+    let avatarResponse = await fetch(`/users/${username}/avatars`);
+    let avatarData = await avatarResponse.json();
+    window.localStorage.setItem("avatar", avatarData.avatar);
+
+    const expirationDate = new Date();
+    expirationDate.setDate(
+      expirationDate.getDate() + 1 * 24 * 60 * 60 * 1000
+    );
+
+    let cookieValue =
+      encodeURIComponent("token") + "=" + encodeURIComponent(data.token);
+    cookieValue += "; expires=" + expirationDate.toUTCString();
+    cookieValue += "; path=/"; // Optional: set the cookie path
+
+    document.cookie = cookieValue;
+
+    navigate("/posts");
   };
 
   const handleUsernameChange = (event) => {
@@ -58,7 +66,6 @@ const LogInForm = ({ navigate }) => {
     window.location.href = "http://localhost:3000/signup";
   };
 
-  
   return (
     <>
       <div className="study-buddy-heading">
@@ -66,29 +73,36 @@ const LogInForm = ({ navigate }) => {
       </div>
       <div className="additional-text">
         <p>
-        In a world where knowledge knows no boundaries, where learning is an eternal quest, a group of exceptional minds has come together to form the ultimate study group. But this is no ordinary group. Their secret weapon is a brilliant, eccentric AI helper that rivals even the legendary Sheldon Cooper.
+          In a world where knowledge knows no boundaries, where learning is an eternal quest, a group of exceptional minds has come together to form the ultimate study group. But this is no ordinary group. Their secret weapon is a brilliant, eccentric AI helper that rivals even the legendary Sheldon Cooper.
         </p>
       </div>
       <div className="container">
         <form onSubmit={handleSubmit} className="form">
-          <div className="input-group">
+          <div className={`input-group ${shakeEffect ? "shake" : ""}`}>
             <input
-              placeholder="Username"
+              placeholder={loginError ? "Clearly you're wrong" : "Username"}
               id="username"
               type="text"
               value={username}
               onChange={handleUsernameChange}
+              className={loginError ? "error" : ""}
             />
           </div>
-          <div className="input-group">
+          <div className={`input-group ${shakeEffect ? "shake" : ""}`}>
             <input
-              placeholder="Password"
+              placeholder={loginError ? "Clearly you're wrong" : "Password"}
               id="password"
               type="password"
               value={password}
               onChange={handlePasswordChange}
+              className={loginError ? "error" : ""}
             />
           </div>
+          {loginError && (
+            <div className="error-message" loginError={loginError}>
+              Clearly you're wrong
+            </div>
+          )}
           <div className="button-group">
             <input
               className="login-button"
@@ -98,7 +112,10 @@ const LogInForm = ({ navigate }) => {
               value="Log in"
             />
             <div className="button-spacing"></div>
-            <button className="create-account-button" onClick={handleSignUpClick}>
+            <button
+              className="create-account-button"
+              onClick={handleSignUpClick}
+            >
               Create new account
             </button>
           </div>
@@ -107,6 +124,6 @@ const LogInForm = ({ navigate }) => {
     </>
   );
 };
-  
+
 
 export default LogInForm;
